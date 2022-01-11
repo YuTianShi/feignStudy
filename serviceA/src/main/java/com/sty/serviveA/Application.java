ca5a419df6b3314129a20bf87cfcb292e241abdd
施天宇 <1508985234@qq.com>
2022-01-11 21:56:14 +0800

    加入空类

---
+++ serviceA/src/main/java/com/sty/serviveA/Application.java
@@ -0,0 +1,9 @@
+package com.sty.serviveA;
+
+/**
+ * Application.
+ *
+ * @author shitianyu 2022-01-11 21:55
+ */
+public class Application {
+}
---
+++ serviceB/src/main/java/com/sty/serviceB/Application.java
@@ -0,0 +1,9 @@
+package com.sty.serviceB;
+
+/**
+ * Application.
+ *
+ * @author shitianyu 2022-01-11 21:55
+ */
+public class Application {
+}
