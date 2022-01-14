87a5452949d3e1c92bc66e4b58ba6893fdadc4d5
施天宇 <1508985234@qq.com>
2022-01-14 14:56:45 +0800

    serviceA配置完毕

--- serviceA/src/main/java/com/sty/serviveA/Application.java
+++ serviceA/src/main/java/com/sty/serviveA/Application.java
@@ -1,9 +1,20 @@
 package com.sty.serviveA;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+
 /**
  * Application.
  *
  * @author shitianyu 2022-01-11 21:55
  */
+@SpringBootApplication
+@EnableFeignClients(basePackages = "com.sty")
 public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
 }
--- serviceB/src/main/java/com/sty/serviceB/Application.java
+++ serviceB/src/main/java/com/sty/serviceB/Application.java
@@ -1,9 +1,20 @@
 package com.sty.serviceB;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
 /**
  * Application.
  *
  * @author shitianyu 2022-01-11 21:55
  */
+@SpringBootApplication
+@EnableFeignClients(basePackages = "com.sty")
 public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
 }
