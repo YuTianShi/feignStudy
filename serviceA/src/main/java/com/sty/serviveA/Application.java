eda14f04adb82e108abc48783f6b06244e599ff3
yutianshi <1508985234@qq.com>
2022-01-16 20:44:06 +0800

    serviceA不报错

--- serviceA/src/main/java/com/sty/serviveA/Application.java
+++ serviceA/src/main/java/com/sty/serviveA/Application.java
@@ -2,6 +2,7 @@ package com.sty.serviveA;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
 /**
@@ -11,6 +12,7 @@ import org.springframework.cloud.netflix.feign.EnableFeignClients;
  */
 @SpringBootApplication
 @EnableFeignClients(basePackages = "com.sty")
+@EnableDiscoveryClient
 public class Application {
 
     public static void main(String[] args) {
